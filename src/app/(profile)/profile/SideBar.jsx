2e2a53be4e0770534bb08374907e3407ebbbfaff
Mahziyar <mgillanpour79@gmail.com>
2024-01-27 16:26:56 +0330

    user panel style

--- src/app/(profile)/profile/SideBar.jsx
+++ src/app/(profile)/profile/SideBar.jsx
@@ -2,6 +2,7 @@
 
 import { logout } from "@/services/authServices";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 function SideBar() {
   const logoutHandler = async () => {
@@ -11,24 +12,38 @@ function SideBar() {
     // localStorage.removeItem("token");
     document.location.href = "/";
   };
+  // const params = useSearchParams()
+  const path = usePathname();
 
   return (
     <div>
-      <ul className="flex flex-col space-y-8">
-        <li>
+      <ul className="flex flex-col space-y-4">
+        <li className="p-2 rounded-md hover:bg-gray-200">
           <Link href="/">صفحه اصلی</Link>
         </li>
-        <li>
+        <li
+          className={`${
+            path === "/profile" && "bg-gray-200 "
+          } p-2 rounded-md hover:bg-gray-200`}
+        >
           <Link href="/profile">داشبورد</Link>
         </li>
-        <li>
+        <li
+          className={`${
+            path === "/profile/me" && "bg-gray-200 "
+          } p-2 rounded-md hover:bg-gray-200`}
+        >
           <Link href="/profile/me">اطلاعات کاربری</Link>
         </li>
-        <li>
+        <li
+          className={`${
+            path === "/profile/payments" && "bg-gray-200"
+          } p-2 rounded-md hover:bg-gray-200`}
+        >
           <Link href="/profile/payments">سفارشات</Link>
         </li>
-        
-        <li>
+
+        <li className="p-2 rounded-md hover:bg-gray-200">
           <button onClick={logoutHandler}>خروج از حساب کاربری</button>
         </li>
       </ul>
